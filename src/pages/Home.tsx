1185076d03ecda6ea5d74140b21e80119c96ee4a
KathrinKath <kathrin.peled@dci-student.org>
2024-12-05 14:03:26 +0100

    filling home and about sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,20 @@
 export function Home() {
-  return <h1>Home</h1>;
+  const backgroundStyle = {
+    backgroundImage: "url('/public/imgs/Background.jpg')",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white" // Optional: to make the text visible on top of the image
+  };
+
+  return (
+    <div style={backgroundStyle}>
+      <h1 className="fs-1 bg-primary p-4 text-center text-white text-uppercase rounded-3">
+        Welcome to our Virtual Shop
+      </h1>
+    </div>
+  );
 }
